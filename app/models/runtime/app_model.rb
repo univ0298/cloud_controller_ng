ad68ccc8dd14f1c45e166a5ee03553942dde8a45
Seth Boyles <sboyles@pivotal.io>
2020-02-25 13:49:41 -0800

    move tests around
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>

--- app/models/runtime/app_model.rb
+++ app/models/runtime/app_model.rb
@@ -56,7 +56,7 @@ module VCAP::CloudController
     serializes_via_json :environment_variables
 
     add_association_dependencies buildpack_lifecycle_data: :destroy
-    # add_association_dependencies kpack_lifecycle_data: :destroy
+    add_association_dependencies kpack_lifecycle_data: :destroy
     add_association_dependencies labels: :destroy
     add_association_dependencies annotations: :destroy
 
