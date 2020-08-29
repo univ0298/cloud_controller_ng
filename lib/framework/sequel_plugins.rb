c3fbf77bbef4aca939c726c7f26b80fdaa91af67
Greg Cobb <gcobb@pivotal.io>
2020-08-28 17:29:53 -0700

    Move plugin initialization to framework
    Authored-by: Greg Cobb <gcobb@pivotal.io>

--- lib/framework/sequel_plugins.rb
+++ lib/framework/sequel_plugins.rb
@@ -3,3 +3,11 @@ require 'framework/sequel_plugins/vcap_serialization'
 require 'framework/sequel_plugins/vcap_normalization'
 require 'framework/sequel_plugins/vcap_relations'
 require 'framework/sequel_plugins/vcap_guid'
+
+Sequel::Model.plugin :vcap_validations
+Sequel::Model.plugin :vcap_serialization
+Sequel::Model.plugin :vcap_normalization
+Sequel::Model.plugin :vcap_relations
+Sequel::Model.plugin :vcap_guid
+
+Sequel::Model.plugin :association_dependencies
--- lib/sequel_plugins/sequel_plugins.rb
+++ lib/sequel_plugins/sequel_plugins.rb
@@ -3,12 +3,6 @@ require 'sequel_plugins/update_or_create'
 require 'sequel_plugins/vcap_user_group'
 require 'sequel_plugins/vcap_user_visibility'
 
-Sequel::Model.plugin :vcap_validations
-Sequel::Model.plugin :vcap_serialization
-Sequel::Model.plugin :vcap_normalization
-Sequel::Model.plugin :vcap_relations
-Sequel::Model.plugin :vcap_guid
 Sequel::Model.plugin :vcap_user_group
 Sequel::Model.plugin :vcap_user_visibility
 Sequel::Model.plugin :update_or_create
-Sequel::Model.plugin :association_dependencies
